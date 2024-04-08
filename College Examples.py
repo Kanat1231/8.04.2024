6e40b816d9364e9554a74a10c1e7533480c65bbb
Kanat1231 <kanatdauletbaj272@gmail.com>
2024-04-08 22:53:21 +0600

    first commit

---
+++ College Examples.py
@@ -0,0 +1,29 @@
+#1
+list = [1,2,3,4,5]
+for i in list:
+    print("Element", list)
+#2
+list = [1,2,3,4,5]
+print(list[0])
+
+#3
+list = [1,2,3,4,5]
+list.append(9)
+print(list)
+
+#4
+for i in range(0,999):
+    print(i)
+#5
+list1 = [1,2,3,21,1]
+print(list1[1])
+#6
+list2 = ["a","b",1,"de"]
+for i in list2:
+    print(i)
+#7
+for i in "ВтиПо":
+    print(i)
+
+
+
